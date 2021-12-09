b29c59857b9134a8ebf20d038d8c6fa722db07ac
Yanis Dreymanis <ydreiman@gmail.com>
2021-12-09 14:14:58 +0300

    Truncate a string

--- jstasks.js
+++ jstasks.js
@@ -31,3 +31,18 @@ function checkSpam(str) {
 // console.log(checkSpam('buy ViAgRA now'));
 // console.log(checkSpam('free xxxxx'));
 // console.log(checkSpam("innocent rabbit"));
+
+// 4. Усечение строки
+// Создайте функцию truncate(str, maxlength), которая проверяет длину строки str и, если она превосходит maxlength, заменяет конец str на "…", так, чтобы её длина стала равна maxlength.
+// Результатом функции должна быть та же строка, если усечение не требуется, либо, если необходимо, усечённая строка.
+// Например:
+// truncate("Вот, что мне хотелось бы сказать на эту тему:", 20) = "Вот, что мне хотело…"
+// truncate("Всем привет!", 20) = "Всем привет!"
+function truncate(str, maxlength) {
+    if ( (str.length - 1) <= maxlength) {
+        return str
+    }
+    return `${str.substr(0, maxlength - 1)}…`;
+}
+console.log(truncate("Вот, что мне хотелось бы сказать на эту тему:", 20));
+console.log(truncate("Всем привет!", 20));
