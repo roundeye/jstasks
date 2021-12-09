20a80d45595596d453a8082dbdded3352c81df54
Yanis Dreymanis <ydreiman@gmail.com>
2021-12-09 15:24:36 +0300

    Truncate a string (refactoring)

--- jstasks.js
+++ jstasks.js
@@ -44,5 +44,11 @@ function truncate(str, maxlength) {
     }
     return `${str.substr(0, maxlength - 1)}…`;
 }
-console.log(truncate("Вот, что мне хотелось бы сказать на эту тему:", 20));
-console.log(truncate("Всем привет!", 20));
+// console.log(truncate("Вот, что мне хотелось бы сказать на эту тему:", 20));
+// console.log(truncate("Всем привет!", 20));
+
+function truncate2(str, maxlength) {
+    return (str.length > maxlength) ? str.slice(0, maxlength - 1) + '…' : str;
+}
+console.log(truncate2("Вот, что мне хотелось бы сказать на эту тему:", 20));
+console.log(truncate2("Всем привет!", 20));
