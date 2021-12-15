681111a2ee2cd43e2970271280fd76037c178f38
Yanis Dreymanis <ydreiman@gmail.com>
2021-12-15 22:48:29 +0300

    bad style

--- jstasks.js
+++ jstasks.js
@@ -255,11 +255,13 @@ function checkAlert() {
 
 function checkLogin(login, pass) {
     if ( (login === 'Админ') && (pass === 'Я главный') ) {
-        return 'Здравствуйте'
+         return 'Здравствуйте'
     }
+
     if ( (login === 'Админ') && (pass !== 'Я главный') ) {
         return 'Неверный пароль'
     }
+
     if (login !== 'Админ') {
         return 'Отменено'
     }
@@ -286,3 +288,42 @@ function checkLogin(login, pass) {
 // } else {
 //   alert('Я вас не знаю')
 // }
+
+// 17. Плохой стиль
+// Какие недостатки вы видите в стиле написания кода этого примера?
+
+// function pow(x,n)
+// {
+//   let result=1;
+//   for(let i=0;i<n;i++) {result*=x;}
+//   return result;
+// }
+
+// let x=prompt("x?",''), n=prompt("n?",'')
+// if (n<=0)
+// {
+//   alert(`Степень ${n} не поддерживается, введите целую степень, большую 0`);
+// }
+// else
+// {
+//   alert(pow(x,n))
+// }
+
+function pow(x, n) {
+  let result = 1;
+
+    for (let i = 0; i < n; i++) {
+        result *= x;
+    }
+
+    return result;
+}
+
+let x = prompt('x?', ''); 
+let n = prompt('n?', '');
+
+  if (n <= 0) {
+  alert(`Степень ${n} не поддерживается, введите целую степень, большую 0`);
+} else {
+  alert( pow(x, n) )
+}
