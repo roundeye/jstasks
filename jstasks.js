8cdd6e177d48a038fdc32f5b24410dd143f8b7a7
Yanis Dreymanis <ydreiman@gmail.com>
2021-12-15 22:50:16 +0300

    bad stle - 2

--- jstasks.js
+++ jstasks.js
@@ -310,8 +310,8 @@ function checkLogin(login, pass) {
 // }
 
 function pow(x, n) {
-  let result = 1;
-
+    let result = 1;
+    
     for (let i = 0; i < n; i++) {
         result *= x;
     }
@@ -322,8 +322,8 @@ function pow(x, n) {
 let x = prompt('x?', ''); 
 let n = prompt('n?', '');
 
-  if (n <= 0) {
-  alert(`Степень ${n} не поддерживается, введите целую степень, большую 0`);
-} else {
-  alert( pow(x, n) )
+    if (n <= 0) {
+        alert(`Степень ${n} не поддерживается, введите целую степень, большую 0`);
+} else { 
+    alert( pow(x, n) );
 }
