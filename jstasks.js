e8e8c3621a909e43cbd52ba0d2059b026783f54a
Yanis Dreymanis <ydreiman@gmail.com>
2021-12-21 19:22:06 +0300

    19. 1-module-2-task "finish the function"

--- jstasks.js
+++ jstasks.js
@@ -362,3 +362,37 @@ function factorial(n) {
 
     return result;
 }
+
+// 19. Допишите функцию валидации имени пользователя
+
+/**
+* Эту функцию трогать не нужно
+*/
+function print(text) {
+ console.log(text);
+}
+
+/**
+* Эту функцию нужно поменять так,
+* чтобы функция sayHello работала корректно
+*/
+
+function isValid(name) {
+    if (name === null) {
+        return false;
+    }
+
+    if (name.length > 3 && name.indexOf(' ') === -1) {
+        return true;
+    }
+}
+
+function sayHello() {
+ let userName = prompt('Введите ваше имя');
+
+ if (isValid(userName)) {
+   print(`Welcome back, ${userName}!`);
+ } else {
+   print('Некорректное имя');
+ }
+}
