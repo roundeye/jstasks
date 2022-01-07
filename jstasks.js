eb3206f26acea19b32ea16ffb167e96ffbac6eaf
Yanis Dreymanis <ydreiman@gmail.com>
2022-01-07 19:58:23 +0300

    30. Create Phone Number

--- jstasks.js
+++ jstasks.js
@@ -542,3 +542,10 @@ function solution(str){
 function removeChar(str){
   return str.slice(1, str.length - 1)
 }
+
+/* 30. Create Phone Number
+Example
+createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890" */
+function createPhoneNumber(numbers){
+  return `(${numbers[0]}${numbers[1]}${numbers[2]}) ${numbers[3]}${numbers[4]}${numbers[5]}-${numbers[6]}${numbers[7]}${numbers[8]}${numbers[9]}`
+}
