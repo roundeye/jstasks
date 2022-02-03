0f0d1dd001d07aa5624c6984b248b41cfb397016
Yanis Dreymanis <ydreiman@gmail.com>
2022-02-03 19:35:44 +0300

    Задачи по объектам: 34, 35, 36, 37, 38 34. Привет, object; 35. Проверка на пустоту; 36. Объекты-константы?; 37. Сумма свойств объекта; 38. Умножаем все числовые свойства на 2

--- jstasks.js
+++ jstasks.js
@@ -602,3 +602,129 @@ function checkAlert() {
   return arr
 }
 // console.log(checkAlert())
+
+/* 34. Привет, object
+Напишите код, выполнив задание из каждого пункта отдельной строкой:
+
+Создайте пустой объект user.
+Добавьте свойство name со значением John.
+Добавьте свойство surname со значением Smith.
+Измените значение свойства name на Pete.
+Удалите свойство name из объекта. */
+
+let emptyObject = {};
+emptyObject['userName'] = 'John';
+emptyObject['surname'] = 'Smith';
+emptyObject['userName'] = 'Pete';
+delete emptyObject['userName'];
+
+// console.log(emptyObject);
+
+/* 35. Проверка на пустоту
+Напишите функцию isEmpty(obj), которая возвращает true, если у объекта нет свойств, иначе false.
+
+Должно работать так:
+
+let schedule = {};
+alert( isEmpty(schedule) ); // true
+schedule["8:30"] = "get up";
+alert( isEmpty(schedule) ); // false */
+
+function isEmpty(obj) {
+  for (let key in obj) {
+    return false;
+  }
+
+  return true;
+}
+
+// console.log(isEmpty({}));
+// console.log(isEmpty({age: 35, surname: 'Ivanov'}));
+
+/* 36. Объекты-константы?
+Можно ли изменить объект, объявленный с помощью const? Как вы думаете?
+
+const user = {
+  name: "John"
+};
+// это будет работать?
+user.name = "Pete"; */
+
+const user = {
+  surname: 'Ivanov'
+};
+
+user['surname'] = 'Pes';
+
+// console.log(user);
+
+/* 37. Сумма свойств объекта
+У нас есть объект, в котором хранятся зарплаты нашей команды:
+
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130
+}
+
+Напишите код для суммирования всех зарплат и сохраните результат в переменной sum. Должно получиться 390.
+Если объект salaries пуст, то результат должен быть 0. */
+
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130
+}
+
+function sumSalaries(obj) {
+  let sum = 0;
+  for (let key in obj) {
+    sum += obj[key]
+  }
+
+  return sum;
+}
+
+// console.log(sumSalaries(salaries));
+
+/* 38. Умножаем все числовые свойства на 2
+Создайте функцию multiplyNumeric(obj), которая умножает все числовые свойства объекта obj на 2.
+
+Например:
+
+// до вызова функции
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu"
+};
+
+multiplyNumeric(menu);
+
+// после вызова функции
+menu = {
+  width: 400,
+  height: 600,
+  title: "My menu"
+};
+Обратите внимание, что multiplyNumeric не нужно ничего возвращать. Следует напрямую изменять объект.
+
+P.S. Используйте typeof для проверки, что значение свойства числовое. */
+
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu"
+};
+
+function multiplyNumeric(obj) {
+  for (let key in obj) {
+    if (typeof obj[key] === 'number') {
+      obj[key] *= 2;
+    }
+  }
+  return menu
+}
+
+// console.log(menu);
+// console.log(multiplyNumeric(menu));
