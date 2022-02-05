caae3147a99dee9acba26b7bcfd22b3c19ae8538
Yanis Dreymanis <ydreiman@gmail.com>
2022-02-05 23:22:39 +0300

    39. Напишите функцию для суммирования всех зарплат.

--- jstasks.js
+++ jstasks.js
@@ -728,3 +728,36 @@ function multiplyNumeric(obj) {
 
 // console.log(menu);
 // console.log(multiplyNumeric(menu));
+
+/* 39. У нас есть объект, в котором хранятся зарплаты нашей команды:
+
+let salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: 'December',
+  currency: 'USD',
+  isPayed: false
+}
+Напишите функцию для суммирования всех зарплат. */
+
+let salariesEmployees = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: 'December',
+  currency: 'USD',
+  isPayed: false
+}
+
+function sumSalary(salaries) {
+  let sum = 0;
+  for (let key in salaries) {
+    if (typeof salaries[key] === 'number' && isFinite(salaries[key])) {
+      sum += salaries[key];
+    }
+  }
+  return sum;
+}
+
+// console.log(sumSalary(salariesEmployees))
