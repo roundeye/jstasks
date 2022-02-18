88d434e8154b936d80b42582d63fe0ebb445dabe
Yanis Dreymanis <ydreiman@gmail.com>
2022-02-18 18:55:11 +0300

    49. Подмассив наибольшей суммы

--- jstasks.js
+++ jstasks.js
@@ -1051,7 +1051,45 @@ function sumInput() {
   return sum;
 }
 
-alert(sumInput()); //работает в браузере, т.к. модальные окна
+// alert(sumInput()); //работает в браузере, т.к. модальные окна
 
+/* 49. Подмассив наибольшей суммы
+На входе массив чисел, например: arr = [1, -2, 3, 4, -9, 6].
 
+Задача: найти непрерывный подмассив в arr, сумма элементов в котором максимальна.
+
+Функция getMaxSubSum(arr) должна возвращать эту сумму.
+
+Например:
+
+getMaxSubSum([-1, 2, 3, -9]) = 5 (сумма выделенных)
+getMaxSubSum([2, -1, 2, 3, -9]) = 6
+getMaxSubSum([-1, 2, 3, -9, 11]) = 11
+getMaxSubSum([-2, -1, 1, 2]) = 3
+getMaxSubSum([100, -9, 2, -3, 5]) = 100
+getMaxSubSum([1, 2, 3]) = 6 (берём все)
+Если все элементы отрицательные – ничего не берём(подмассив пустой) и сумма равна «0»:
+
+getMaxSubSum([-1, -2, -3]) = 0
+Попробуйте придумать быстрое решение: O(n2), а лучше за О(n) операций. */
+
+function getMaxSubSum(arr) {
+  
+  let maxSumNumbers = [];
+
+  for (let j = 0; j < arr.length; j++) {
+    let sum = 0;
+
+    for (let i = j; i < arr.length; i++) {
+      sum = sum + arr[i];
+      maxSumNumbers.push(sum);
+    }
+  }
+
+  if (Math.max.apply(null, maxSumNumbers) < 0) return 0
+  return Math.max.apply(null, maxSumNumbers)
+}
+
+console.log(getMaxSubSum([0, 1, 2, 3]));
+console.log(getMaxSubSum([-1, -2, -3, -4]));
 
