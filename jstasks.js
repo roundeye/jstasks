02b69518f3c03fb7fd1a716ac9386a9ad8bbfe97
Yanis Dreymanis <ydreiman@gmail.com>
2022-03-17 17:22:00 +0300

    54. Фильтрация по диапазону

--- jstasks.js
+++ jstasks.js
@@ -1354,4 +1354,29 @@ function camelize(str) {
 return arr.join('')
 }
 
-console.log(camelize('list-style-image'))
+// console.log(camelize('list-style-image'))
+
+/* 54. Фильтрация по диапазону
+Напишите функцию filterRange(arr, a, b), которая принимает массив arr, ищет в нём элементы между a и b и отдаёт массив этих элементов.
+Функция должна возвращать новый массив и не изменять исходный.
+
+Например:
+
+let arr = [5, 3, 8, 1];
+let filtered = filterRange(arr, 1, 4);
+
+alert( filtered ); // 3,1 (совпадающие значения)
+alert( arr ); // 5,3,8,1 (без изменений) */
+
+function filterRange(arr, a, b) {
+  let resultArr = [];
+  for (let x of arr) {
+    if (x <= b && x >= a) {
+      resultArr.push(x)
+    } 
+  }
+
+  return resultArr
+}
+
+console.log(filterRange([5, 3, 8, 1], 1, 4))
