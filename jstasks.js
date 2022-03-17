7857dd22d3b34ee86ede3ec61a8271aac19263a9
Yanis Dreymanis <ydreiman@gmail.com>
2022-03-17 16:32:49 +0300

    53. Переведите текст вида border-left-width в borderLeftWidth

--- jstasks.js
+++ jstasks.js
@@ -1332,3 +1332,26 @@ let soldiers = userss.filter(army.canJoin, army);
 // console.log(soldiers.length); // 2
 // console.log(soldiers[0].age); // 20
 // console.log(soldiers[1].age); // 23
+
+/* 53. Переведите текст вида border-left-width в borderLeftWidth
+Напишите функцию camelize(str), которая преобразует строки вида «my-short-string» в «myShortString».
+
+То есть дефисы удаляются, а все слова после них получают заглавную букву.
+
+Примеры:
+
+camelize("background-color") == 'backgroundColor';
+camelize("list-style-image") == 'listStyleImage';
+camelize("-webkit-transition") == 'WebkitTransition';
+P.S. Подсказка: используйте split, чтобы разбить строку на массив символов, потом переделайте всё как нужно и методом join соедините обратно. */
+
+function camelize(str) {
+  let arr = str.split('-');
+  for (let i = 1; i < arr.length; i++) {
+    arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1);
+  }
+  
+return arr.join('')
+}
+
+console.log(camelize('list-style-image'))
