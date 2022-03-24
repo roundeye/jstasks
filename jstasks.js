346c9b01f127906659d9c367b279fa5b63cc8b74
Yanis Dreymanis <ydreiman@gmail.com>
2022-03-24 13:52:15 +0300

    60. Отсортировать пользователей по возрасту

--- jstasks.js
+++ jstasks.js
@@ -1530,4 +1530,36 @@ let names3 = [ Dasha, Pes, Den ];
 
 let newNames3 = names3.map(x => ({fullName: `${x.name} ${x.surname}`, id: x.id}))
 
-console.log(newNames3)
+// console.log(newNames3)
+
+/* 60. Отсортировать пользователей по возрасту
+Напишите функцию sortByAge(users), которая принимает массив объектов со свойством age и сортирует их по нему.
+
+Например:
+
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 28 };
+
+let arr = [ vasya, petya, masha ];
+
+sortByAge(arr);
+
+// теперь: [vasya, masha, petya]
+alert(arr[0].name); // Вася
+alert(arr[1].name); // Маша
+alert(arr[2].name); // Петя */
+
+let pes = [ { name: "Вася", age: 25 }, { name: "Петя", age: 30 }, { name: "Маша", age: 28 } ];
+
+// function sortByAge(arr) {
+//   // arr.sort((a, b) => a.age > b.age ? 1 : -1)
+//   arr.sort((a, b) => a.age - b.age);
+// }
+
+function sortByAge(arr) {
+  arr.sort((a, b) => a.age > b.age ? 1 : -1);
+  return arr
+}
+
+console.log(sortByAge(pes))
