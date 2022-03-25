eec6523e93bc4165ea61b10245bb78f381dbc58e
Yanis Dreymanis <ydreiman@gmail.com>
2022-03-25 12:34:29 +0300

    63. - решение без второго массива

--- jstasks.js
+++ jstasks.js
@@ -1680,3 +1680,15 @@ let strings = ["кришна", "кришна", "харе", "харе",
 ];
 
 console.log(unique(strings))
+
+function unique2(arr) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+      if (!result.includes(arr[i])) {
+        result.push(arr[i])
+      }
+  }
+  return result
+}
+
+console.log(unique2(strings))
