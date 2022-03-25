06ff82048f18337516f610452966e9d236fd3d08
Yanis Dreymanis <ydreiman@gmail.com>
2022-03-25 12:30:36 +0300

    63. Оставить уникальные элементы массива

--- jstasks.js
+++ jstasks.js
@@ -1643,4 +1643,40 @@ function getAverageAge3(users) {
   return users.reduce((sum, current) => sum + current.age, 0) / users.length
 }
 
-console.log(getAverageAge3(arr7))
+// console.log(getAverageAge3(arr7))
+
+/* 63. Оставить уникальные элементы массива
+
+Пусть arr – массив строк.
+
+Напишите функцию unique(arr), которая возвращает массив, содержащий только уникальные элементы arr.
+
+Например:
+
+function unique(arr) {
+ 
+}
+
+let strings = ["кришна", "кришна", "харе", "харе",
+  "харе", "харе", "кришна", "кришна", ":-O"
+];
+
+alert( unique(strings) ); // кришна, харе, :-O */
+
+function unique(arr) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 1; j < arr.length; j++) {
+      if (arr[i] === arr[j] && !result.includes(arr[i])) {
+        result.push(arr[i])
+      }
+    }
+  }
+  return result
+}
+
+let strings = ["кришна", "кришна", "харе", "харе",
+  "харе", "харе", "кришна", "кришна", ":-O"
+];
+
+console.log(unique(strings))
