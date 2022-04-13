6217389e47c6827d3c911f97e2131228bdd1c45b
Yanis Dreymanis <ydreiman@gmail.com>
2022-04-13 19:53:54 +0300

    Дочерние элементы в DOM

---
+++ 4-module/4-task/index.js
@@ -0,0 +1,6 @@
+function makeDiv() {
+  let pes = document.createElement('div')
+  // pes.textContent = 'DEN PES';
+  pes.innerHTML = '<strong>Новый текст ссылки с оберткой</strong>'
+  return pes
+}
